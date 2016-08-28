096ef0b45153a55cf9018db6306acab12eeed2bd
Sean Ellis <seane235@gmail.com>
2016-08-28 14:05:32 -0400

    updating dropdowns

--- public/js/main.js
+++ public/js/main.js
@@ -3,35 +3,42 @@
    * mwt = singleton (M)ediabrix (W)eb (T)ester
    */
   const mwt = scope.mwt = {};
+  mwt.dropdowns = {};
   mwt.init = function() {
-    // this.updateMeta();
     this.bindEvents();
     this.setMeta();
+    this.updateDropdowns();
   };
 
   mwt.bindEvents = function() {
     var dropdowns = document.querySelectorAll('.dropdown');
     for (var i = 0; i < dropdowns.length; i++) {
+      var currentDropdown = dropdowns[i];
+      mwt.dropdowns[currentDropdown.dataset.type] = currentDropdown;
       dropdowns[i].addEventListener('change', this.onDropdownChange);
     }
   };
 
   mwt.onDropdownChange = function(e) {
-    var dropdown = e.target;
-    var type = dropdown.dataset.type;
+    const dropdown = e.target;
+    const type = dropdown.dataset.type;
   };
 
   mwt.setMeta = function() {
-    var pathname = window.location.pathname.substring(1);
-    var split = pathname.split('/');
-    var env = split[0];
-    var product = split[1];
-    console.log('test ~~>', env);
-    console.log('test ~~>', product);
+    const pathname = window.location.pathname.substring(1);
+    const split = pathname.split('/');
+    const environment = split[0];
+    const product = split[1];
+    const adType = split[2];
+    mwt.meta = { environment, product, adType };
   };
 
-
-
+  mwt.updateDropdowns = function() {
+    for (var key in this.dropdowns) {
+      const dropdown = this.dropdowns[key];
+      dropdown.value = mwt.meta[key];
+    }
+  };
 
   scope.addEventListener('DOMContentLoaded', function() {
     mwt.init();
